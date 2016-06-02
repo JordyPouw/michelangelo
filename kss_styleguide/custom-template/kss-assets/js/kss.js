bd4c9379ef8fe4ddcbb102b525ea9bc3fa0a7fa6
JordyPouw <jordypouw@gmail.com>
2016-06-03 00:52:55 +0200

    fix scroll to navigation, add compiled css

--- kss_styleguide/custom-template/kss-assets/js/kss.js
+++ kss_styleguide/custom-template/kss-assets/js/kss.js
@@ -82,25 +82,23 @@
   });
 
 
-  // // smooth scrolling.
-  // smoothScrolling();
-  
-  // function smoothScrolling () {
-  //   $('a[href*=#]:not([href=#])').on('click', function (e) {
-  //     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-  //       var target = $(this.hash);
-  //       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-        
-  //       if (target.length) {
-  //         $('html, body').animate({
-  //           scrollTop: target.offset().top
-  //         }, 1000);
-          
-  //         return false;
-  //       }
-  //     }
-  //   });
-  // }
+  // smooth scrolling.
+  (function smoothScrolling () {
+    $('.kss-nav__item > a[href*=section]').on('click', function (e) {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+
+          return false;
+        }
+      }
+    });
+  })();
 
 
 })();
